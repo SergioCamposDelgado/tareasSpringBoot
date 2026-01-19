aeaf489e52c14dd248f40942eb8ff8df775b783e
SergioCamposDelgado <campos.delgado.sergio@iescamas.es>
2026-01-19 10:29:52 +0100

    Añadido el proyeto base junto a sus dependencias en pom.xml

---
+++ ra3/src/main/java/com/example/ra3/Ra3Application.java
@@ -0,0 +1,14 @@
+package com.example.ra3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ra3Application {
+
+	//App creada por Sergio Campos Delgado
+	public static void main(String[] args) {
+		SpringApplication.run(Ra3Application.class, args);
+	}
+
+}
